d4667474d05e7e7646b399e8743203afdd7049a7
EngrHumza <humzaliaqat01@gmail.com>
2020-06-07 16:53:04 +0500

    ass5

---
+++ Assignment#1/app.js
@@ -0,0 +1,14 @@
+// Task#1
+
+alert("Welcome to GreenTech");
+
+// Task#2
+
+alert("Error!Please enter a valid password");
+
+// Task#3
+
+alert("Welcome to JS Land..." + '\n' + "Happy Coding!");
+
+// Task#4
+
---
+++ Assignment#2/app.js
@@ -0,0 +1,28 @@
+var userNAme;
+
+var myName;
+
+myName="Humza Liaqat";
+
+var message;
+message="Hello World";
+alert(message);
+
+var name;
+var age;
+var certified;
+
+name="Ali";
+age=15;
+certified="Certified Mobile Application Development";
+alert(name);
+alert(age);
+alert(certified);
+
+var email;
+email="humzaliaqat01@gmail.com";
+alert("My email address is" + email);
+
+var book;
+book="A smarter way to learn JavaScript";
+alert("I am trying to learn from the book" + book);
